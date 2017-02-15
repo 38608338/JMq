e35b863c654bd04801deb4d4c36e4481a88b2e24
leo <38608338@qq.com>
2017-02-15 17:31:34 +0800

    bug1

--- JMq/src/leo/Sender.java
+++ JMq/src/leo/Sender.java
@@ -17,9 +17,12 @@ public class Sender {
 	String returnXml = null;
 
 	public static void main(String[] args) {
+		singlethread();
+	}
+
+	public static void multithread() {
 		final Sender ss = new Sender();
-		ss.start();
-		for (int i = 1; i < 100; i++) {
+		for (int i = 1; i < 10; i++) {
 			Thread tt=new Thread(new Runnable() {
 				public void run() {
 					for (int j = 0; j < 10; j++) {
@@ -34,14 +37,13 @@ public class Sender {
 				e.printStackTrace();
 			}*/
 		}
-		//ss.stop();
 	}
 
-	public void start() {
-
-	}
-
-	public void stop() {
+	public static void singlethread() {
+		final Sender ss = new Sender();
+		for (int i = 1; i < 100; i++) {
+			ss.Send("work" + i);
+		}
 	}
 
 	public String Send(String xmlData) {
@@ -74,15 +76,34 @@ public class Sender {
 			// 设置不持久化，此处学习，实际根据项目决定
 			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
 			producer.setPriority(1);
-			TextMessage message = session.createTextMessage(xmlData);
+			TextMessage msg = session.createTextMessage(xmlData);
 			// 发送消息到目的地方
 			// System.out.println("外网发送消息：" +xmlData);
 			String msgid = "12432";
-			message.setJMSCorrelationID(msgid);
-			producer.send(message);
+			msg.setJMSCorrelationID(msgid);
+			producer.send(msg);
 			session.commit();
 
-			returnXml = Receive(msgid);
+			// 获取操作连接
+			session = connection.createSession(Boolean.FALSE,
+					Session.CLIENT_ACKNOWLEDGE);
+			destination = session.createQueue("QUEUE_LOCAL2");
+			MessageConsumer consumer = session.createConsumer(destination);
+			// 设置接收者接收消息的时间，为了便于测试，这里谁定为1s
+			Message message = consumer.receive(10000);
+			if (null != message) {
+				if (message instanceof TextMessage) {
+					TextMessage txtMsg = (TextMessage) message;
+					returnXml = txtMsg.getText();
+					// System.out.println("外网收到消息:" + msg);
+
+					// 确认接收，并成功处理了消息
+					//if (txtMsg.getJMSCorrelationID().equals(msgid)) {
+						message.acknowledge();
+					//}
+				}
+			}
+			//returnXml = Receive(msgid);
 			System.out.println("外网消息 发送：" + xmlData + " 返回：" + returnXml);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -94,62 +115,5 @@ public class Sender {
 			}
 		}
 		return returnXml;
-	}
-
-	private String Receive(String msgid) {
-		// ConnectionFactory ：连接工厂，JMS 用它创建连接
-		ConnectionFactory connectionFactory;
-		// Connection ：JMS 客户端到JMS Provider 的连接
-		Connection connection = null;
-		// Session： 一个发送或接收消息的线程
-		Session session;
-		// Destination ：消息的目的地;消息发送给谁.
-		Destination destination;
-		// MessageProducer：消息发送者
-		MessageConsumer consumer;
-		// 构造ConnectionFactory实例对象，此处采用ActiveMq的实现jar
-		connectionFactory = new ActiveMQConnectionFactory(
-				ActiveMQConnection.DEFAULT_USER,
-				ActiveMQConnection.DEFAULT_PASSWORD, "tcp://localhost:61616");
-		try {
-			// 构造从工厂得到连接对象
-			connection = connectionFactory.createConnection();
-			// 启动
-			connection.start();
-			// 获取操作连接
-			session = connection.createSession(Boolean.FALSE,
-					Session.AUTO_ACKNOWLEDGE);
-			// 获取session注意参数值xingbo.xu-queue是一个服务器的queue，须在在ActiveMq的console配置
-			destination = session.createQueue("QUEUE_LOCAL2");
-			consumer = session.createConsumer(destination);
-			while (true) {
-				// 设置接收者接收消息的时间，为了便于测试，这里谁定为1s
-				Message message = consumer.receive(1000);
-				if (null != message) {
-					if (message instanceof TextMessage) {
-						TextMessage txtMsg = (TextMessage) message;
-						String msg = txtMsg.getText();
-						// System.out.println("外网收到消息:" + msg);
-
-						// 确认接收，并成功处理了消息
-						if (txtMsg.getJMSCorrelationID().equals(msgid)) {
-							message.acknowledge();
-							return msg;
-						}
-					}
-				} else {
-					break;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (null != connection)
-					connection.close();
-			} catch (Throwable ignore) {
-			}
-		}
-		return "超时";
 	}
 }
